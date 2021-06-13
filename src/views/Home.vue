cd9e42217ffc3c203c7ea0156df24672015bd0e0
Muhammad Adlan Jazuli <adlanjazuli@gmail.com>
2021-06-13 19:44:07 +0700

    commit pertama lagi

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HeaderMaj />
+    <HeroMaj />
+    <WomenMaj />
+    <InstaMaj />
+    <PartnerMaj />
+    <FooterMaj />
+
+    
+  </div>
+</template>
+
+<script>
+import HeaderMaj from '@/components/HeaderMaj.vue'
+import HeroMaj from '@/components/HeroMaj.vue'
+import WomenMaj from '@/components/WomenMaj.vue'
+import InstaMaj from '@/components/InstaMaj.vue'
+import PartnerMaj from '@/components/PartnerMaj.vue'
+import FooterMaj from '@/components/FooterMaj.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderMaj,
+    HeroMaj,
+    WomenMaj,
+    InstaMaj,
+    PartnerMaj,
+    FooterMaj,
+  }, 
+  
+}
+</script>
